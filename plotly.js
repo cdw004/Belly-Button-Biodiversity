5f530919c8490ddec1100f3d3ff909da0434ea37
Christopher D. Wong <34668473+cdw004@users.noreply.github.com>
2018-05-23 16:15:50 -0700

    Update JS

---
+++ plotly.js
@@ -0,0 +1,22 @@
+function getSampleNames(){
+    var selector = document.getElementById('selDataset');
+    var url = "/names";
+    Plotly.d3.json(url, function(error, response) {
+        if (error) return console.warn(error);
+        var data = response;
+        data.map(function(sample){
+            var option = document.createElement('option')
+            option.text = sample
+            option.value = sample
+            selector.appendChild(option)
+        });
+    });
+};
+
+getSampleNames();
+
+function optionChanged(sample){
+    updatePie(sample);
+    updateBubble(sample);
+    updateMetadata(sample);
+};
